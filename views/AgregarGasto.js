8a9eb21dff814c92a5ba9acf699f3662f729f722
Michelle Ticchetti <mticchetti@gmail.com>
2022-11-23 11:00:59 -0300

    prueba

--- views/AgregarGasto.js
+++ views/AgregarGasto.js
@@ -1,37 +1,36 @@
 import {
-    NativeBaseProvider,
-    Box,
-    Button,
-    HStack,
-    Input,
-    Stack,
-    Switch,
-    Text,
-  } from "native-base";
-  
-  export default function AgregarGasto({ navigation }) {
-    return (
-      <NativeBaseProvider>
-      
-        <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-          <Stack space={4} w="75%" maxW="300px" mx="auto">
-            <Input size="md" placeholder="Monto" />
-            <Input size="md" placeholder="Descripción" />
-          </Stack>
-  
-          <Button
-            success
-            margin={1}
-            onPress={() => {
-              console.log(
-                "ACA TNEMOSQUE LLAMAR A SERVICIOS POST Y PASAR TODS ESTOS PARAMETROS"
-              );
-            }}
-          >
-            <Text>Agregar Gasto</Text>
-          </Button>
-        </Box>
-      </NativeBaseProvider>
-    );
-  }
-  
+  NativeBaseProvider,
+  Box,
+  Button,
+  HStack,
+  Input,
+  Stack,
+  Switch,
+  Text,
+} from "native-base";
+
+export default function AgregarGasto({ navigation }) {
+  return (
+    <NativeBaseProvider>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Stack space={4} w="75%" maxW="300px" mx="auto">
+          <Input size="md" placeholder="Monto" />
+          <Input size="md" placeholder="Descripción" />
+        </Stack>
+
+        <Button
+          success
+          margin={1}
+          onPress={() => {
+            console.log(
+              "ACA TNEMOSQUE LLAMAR A SERVICIOS POST Y PASAR TODS ESTOS PARAMETROS"
+            );
+            console.log("prueba de commit");
+          }}
+        >
+          <Text>Agregar Gasto</Text>
+        </Button>
+      </Box>
+    </NativeBaseProvider>
+  );
+}
